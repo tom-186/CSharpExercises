0e928d24520368a0452a68a60b40601c028df3c0
Tommaso Manni <tommaso.manni@gmail.com>
2020-12-11 17:52:49 +0100

    Solved the seventh exercise: closes #7

---
+++ Misc/ExLINQ.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExLINQ
+{
+    class FilteringUtils
+    {   
+        // Methods
+        public static string Search(string startLetter)
+        {
+            var cities = new string[]{"Arezzo", "Arezzo", "Siena", "Firenze", "Lucca"};
+
+            IEnumerable<string> query =
+            from city in cities
+            where city[0] == startLetter[0]
+            select city;
+            
+            return query.First();
+                     
+        } 
+    }
+}
---
+++ Misc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExLINQ;
+
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Search city which name starts with letter:");
+            string startLetter = Console.ReadLine(); 
+
+            var result = FilteringUtils.Search(startLetter);               
+            
+            Console.WriteLine(result);
+        }
+    }
+}
